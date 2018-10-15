0f91231dbc6d23a8664c359a8590b8fab0a46c84
Xinhe <lixinhe126@126.com>
2018-10-14 20:46:40 -0400

    adjust logical select operator

--- src/test/java/logical/operator/SelectOperatorTest.java
+++ src/test/java/logical/operator/SelectOperatorTest.java
@@ -1,41 +1,44 @@
 package logical.operator;
 
-import model.Tuple;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import org.junit.Test;
+import java.util.Map;
+import java.util.HashMap;
+
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
 import java.io.StringReader;
 
 public class SelectOperatorTest {
+    SelectOperator selectOp;
 
-    @Test
-    public void getNextTuple() throws Exception {
+    public SelectOperatorTest() throws Exception{
         String statement = "SELECT * FROM Boats AS BT WHERE BT.E = 2;";
         CCJSqlParserManager parserManager = new CCJSqlParserManager();
         PlainSelect plainSelect = (PlainSelect) ((Select) parserManager.parse(new StringReader(statement))).getSelectBody();
         Operator scanOp = new ScanOperator(plainSelect, 0);
-
-        Operator selectOp = new SelectOperator(scanOp, plainSelect);
-
-        Tuple tuple = selectOp.getNextTuple();
-        while(tuple != null){
-            assertEquals(2, tuple.getDataAt(1));
-            System.out.println(tuple);
-            tuple = selectOp.getNextTuple();
-        }
+        selectOp = new SelectOperator(scanOp, plainSelect);
+    }
+    @Test
+    public void getExpressionTest()  {
+        Assert.assertEquals("Expression: ", "BT.E = 2", selectOp.getExpression().toString());
     }
 
     @Test
-    public void reset() {
-    }
-
-    @Test
-    public void dump() {
+    public void getChildrenTest()  {
+        Assert.assertEquals("Children: ", 1, selectOp.getChildren().length);
     }
 
     @Test
     public void getSchema() {
+        Map<String, Integer> expectedSchema = new HashMap<>();
+        expectedSchema.put("BT.D", 0);
+        expectedSchema.put("BT.E", 1);
+        expectedSchema.put("BT.F", 2);
+        Assert.assertEquals(expectedSchema, selectOp.getSchema());
+
     }
 }
