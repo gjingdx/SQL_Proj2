091cb1a18bbd6b209e0d87cf5bef2ba55dd5bf82
Xinhe <lixinhe126@126.com>
2018-10-14 20:27:40 -0400

    add schema

--- src/main/java/logical/operator/Operator.java
+++ src/main/java/logical/operator/Operator.java
@@ -1,15 +1,7 @@
 package logical.operator;
 
 import com.sql.interpreter.PhysicalPlanBuilder;
-import model.BufferStateWrapper;
-import model.Tuple;
-import model.TupleWriter;
-import util.Catalog;
-import util.Constants;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.Map;
 
 /**
