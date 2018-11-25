978b99258025d6b2b06df16083c7225ece30cfcc
jing <jingguo7@outlook.com>
2018-11-24 22:11:15 -0500

    logic builder before merge with master

--- src/main/java/logical/interpreter/LogicalPlanBuilder.java
+++ src/main/java/logical/interpreter/LogicalPlanBuilder.java
@@ -5,7 +5,10 @@ import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import util.JoinExpressionVisitor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Handler class to parse SQL, construct query plan and handle initialization
@@ -31,38 +34,82 @@ public class LogicalPlanBuilder {
     * @return
     */
     public static Operator constructLogicalPlanTree(PlainSelect plainSelect) {
-        int tableCount;
-        Operator opLeft;
+        // old constructLogicalPlanTree
+//        int tableCount;
+//        Operator opLeft;
+//        if (plainSelect.getJoins() == null) {
+//            tableCount = 1;
+//        } else {
+//            tableCount = 1 + plainSelect.getJoins().size();
+//        }
+//
+//        opLeft = new ScanOperator(plainSelect, 0);
+//        if (hasRelatedExpression(opLeft.getSchema(), plainSelect)) {
+//            opLeft = new SelectOperator(opLeft, plainSelect);
+//        }
+//
+//        for (int i = 1; i < tableCount; ++i) {
+//            Operator opRight = new ScanOperator(plainSelect, i);
+//            if (hasRelatedExpression(opRight.getSchema(), plainSelect)) {
+//                opRight = new SelectOperator(opRight, plainSelect);
+//            }
+//            opLeft = new JoinOperator(opLeft, opRight, plainSelect);
+//        }
+//        if (plainSelect.getSelectItems() != null
+//                && plainSelect.getSelectItems().size() > 0
+//                && plainSelect.getSelectItems().get(0).toString() != "*")
+//            opLeft = new ProjectOperator(opLeft, plainSelect);
+//        if (plainSelect.getDistinct() != null) {
+//            opLeft = new SortOperator(opLeft, plainSelect);
+//            opLeft = new DuplicateEliminationOperator(opLeft);
+//        } else {
+//            if (plainSelect.getOrderByElements() != null)
+//                opLeft = new SortOperator(opLeft, plainSelect);
+//        }
+//        return opLeft;
+
+        // get number of tables used in total which is num of scanOp
+        int numTable = 0;
         if (plainSelect.getJoins() == null) {
-            tableCount = 1;
+            numTable = 1;
         } else {
-            tableCount = 1 + plainSelect.getJoins().size();
+            numTable = 1 + plainSelect.getJoins().size();
         }
 
-        opLeft = new ScanOperator(plainSelect, 0);
-        if (hasRelatedExpression(opLeft.getSchema(), plainSelect)) {
-            opLeft = new SelectOperator(opLeft, plainSelect);
-        }
+        //List<Operator> scanOps = new ArrayList<>(); // list of scan Ops
+        // list of select Ops or scan Op that pass to JoinOp
+        List<Operator> selectOps = new ArrayList<>();
 
-        for (int i = 1; i < tableCount; ++i) {
-            Operator opRight = new ScanOperator(plainSelect, i);
-            if (hasRelatedExpression(opRight.getSchema(), plainSelect)) {
-                opRight = new SelectOperator(opRight, plainSelect);
+        Set<String> attributes = UnionFind.getAttributeSet();
+        for (int i = 0; i < numTable; i++) {
+            Operator logicOp = new ScanOperator(plainSelect, i);
+            //scanOps.add(scan);
+            for (String attribute : attributes) {
+                if (logicOp.getSchema().containsKey(attribute)) {
+                    logicOp = new SelectOperator(logicOp, attribute, UnionFind.find(attribute));
+                }
             }
-            opLeft = new JoinOperator(opLeft, opRight, plainSelect);
+            selectOps.add(logicOp);
+        }
+
+        Operator logicOp;
+        if (selectOps.size() > 1) {
+            logicOp = new JoinOperator(selectOps, plainSelect);
+        } else {
+            logicOp = selectOps.get(0);
         }
         if (plainSelect.getSelectItems() != null
                 && plainSelect.getSelectItems().size() > 0
                 && plainSelect.getSelectItems().get(0).toString() != "*")
-            opLeft = new ProjectOperator(opLeft, plainSelect);
+            logicOp = new ProjectOperator(logicOp, plainSelect);
         if (plainSelect.getDistinct() != null) {
-            opLeft = new SortOperator(opLeft, plainSelect);
-            opLeft = new DuplicateEliminationOperator(opLeft);
+            logicOp = new SortOperator(logicOp, plainSelect);
+            logicOp = new DuplicateEliminationOperator(logicOp);
         } else {
             if (plainSelect.getOrderByElements() != null)
-                opLeft = new SortOperator(opLeft, plainSelect);
+                logicOp = new SortOperator(logicOp, plainSelect);
         }
-        return opLeft;
+        return logicOp;
     }
 
     private static boolean hasRelatedExpression(Map<String, Integer> schemaMap, PlainSelect plainSelect) {
