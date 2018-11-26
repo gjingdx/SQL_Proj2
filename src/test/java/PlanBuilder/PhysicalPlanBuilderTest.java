d40a3624c60bf38a18efb35a6ce51c8c0b3530c4
Xinhe <lixinhe126@126.com>
2018-11-25 21:30:34 -0500

    fix stat bug & unclustered index scan judgement

--- src/test/java/PlanBuilder/PhysicalPlanBuilderTest.java
+++ src/test/java/PlanBuilder/PhysicalPlanBuilderTest.java
@@ -1,6 +1,7 @@
 package PlanBuilder;
 
 import PlanBuilder.PhysicalPlanBuilder;
+import com.sql.interpreter.Handler;
 import logical.operator.*;
 import model.Tuple;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
@@ -8,6 +9,7 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import operator.PhysicalOperator;
 import org.junit.Test;
+import util.Catalog;
 import util.Constants;
 
 import java.io.BufferedReader;
