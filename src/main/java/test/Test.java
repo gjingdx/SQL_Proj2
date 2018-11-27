5fed0b17dce168b0ce112255354315d9918a2a58
Xinhe <lixinhe126@126.com>
2018-11-27 13:16:40 -0500

    test version

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,7 +3,6 @@ package test;
 import com.sql.interpreter.Handler;
 import io.BinaryTupleReader;
 import model.Tuple;
-import util.Catalog;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,7 +10,10 @@ import java.util.Set;
 class Test {
     public static void main(String[] args) {
         try{
-            Handler.init(args);
+            args = new String[2];
+            args[0] = "../project5/expected_output/query";
+            args[1] = "../project5/output/query";
+            //Handler.init(args);
             cmpResult(args[0], args[1]);
         } catch (Exception e) {
             e.printStackTrace();
@@ -21,7 +23,7 @@ class Test {
     // path : xxx/query
     private static void cmpResult(String expectedPath, String outputPath) throws Exception{
         for (int index = 1; index <= 20; ++index) {
-            String outfile = Catalog.getInstance().getOutputPath() + String.valueOf(index);
+            String outfile = outputPath + String.valueOf(index);
             String expectOutputfile = expectedPath + String.valueOf(index);
             BinaryTupleReader r1 = new BinaryTupleReader(outfile);
             BinaryTupleReader r2 = new BinaryTupleReader(expectOutputfile);
